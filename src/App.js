2794424296f4c245ae3d1b5fe83ed8b1c9ca2756
Sengkue <skvgithut@gmail.com>
2023-11-07 15:01:21 +0700

    use fetch data

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import { useEffect } from 'react';
 import Navbar from './components/Navbar';
 import CartContainer from './components/CartContainer';
 import { useSelector, useDispatch } from 'react-redux';
-import { calculateTotals } from './features/Cart/CartSlice';
+import { calculateTotals, getCartItems } from './features/Cart/CartSlice';
 import Modal from './components/Modal';
 
 function App() {
-  const { isOpen } = useSelector((state) => state.modal);
+  const { isOpen, isLoading } = useSelector((state) => state.modal);
   const { cartItems } = useSelector((state) => state.cart);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(calculateTotals());
   }, [cartItems]);
+  
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, []);
 
   return (
     <main>
